3a5eb64a6754a928e99530f028a53d5b120e720c
datguysheepy <36929645+datguysheepy@users.noreply.github.com>
2020-02-14 22:19:45 +0100

    fixes and mobile nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,35 +1,17 @@
 import React from 'react';
 import BrandBig from './BrandBig'
-import ButtonBorder from './ButtonBorder';
+import Nav from './Nav';
 
 import './styles/Header.css';
 
 
 export default function Header() {
 	return (
-		<header className='headerWrapper'>
-			<a href='' className="brandHref">
+		<header className='header-wrapper'>
+			<a href='' className="brand-href">
 				<BrandBig/>
 			</a>
-			
-			<nav className='navWrapper'>
-				<ul>
-					<li><a href='#pricing'>Pricing</a></li>
-					<li><a href='#features'>Features</a></li>
-					<li><a href='#'>FAQ</a></li>
-					<li>
-						<ButtonBorder
-							href = '#'
-							text = 'Go to the app'
-							display = 'inline'
-							bSize = '2px'
-							bColor = 'var(--brand)'
-							className = 'nav-app'
-						/>
-					</li>
-					<li><a href='#'>English</a></li>
-				</ul>
-			</nav>
+			<Nav />
 		</header>
 	)
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import ButtonBorder from './ButtonBorder';
+import { Menu, X, FileText } from 'react-feather';
+
+import './styles/Nav.css';
+
+
+export default function Nav() {
+	return (
+		<nav className='nav-wrapper'>
+			<ul className='nav-content'>
+				<li><a href='#pricing'>Pricing</a></li>
+				<li><a href='#features'>Features</a></li>
+				<li><a href='#'>FAQ</a></li>
+				<li>
+					<ButtonBorder
+						href = '#'
+						text = 'Go to the app'
+						display = 'inline'
+						bSize = '2px'
+						bColor = 'var(--brand)'
+						className = 'nav-app'
+					/>
+				</li>
+				<li><a href='#'>English</a></li>
+			</ul>
+			<div className='nav-wrapper-mobile'>
+				<Menu size={28} onClick={() => {
+					let mobile = document.getElementsByClassName('nav-content-mobile')[0];
+					mobile.style.display = mobile.style.display === 'flex' ? 'none' : 'flex';
+				}}/>
+				<ul className='nav-content-mobile'>
+					<X className='nav-mobile-close' size={28} onClick={() => {
+						let mobile = document.getElementsByClassName('nav-content-mobile')[0];
+						mobile.style.display = mobile.style.display === 'none' ? 'flex' : 'none';
+					}}/>
+					<li><a href='#pricing'>Pricing</a></li>
+					<li><a href='#features'>Features</a></li>
+					<li><a href='#'>FAQ</a></li>
+					<li>
+						<ButtonBorder
+							href = '#'
+							text = 'Go to the app'
+							display = 'inline'
+							bSize = '0px'
+							bColor = 'var(--brand)'
+						/>
+					</li>
+					<li><a href='#'>English</a></li>
+				</ul>
+			</div>
+		</nav>
+	)
+}
