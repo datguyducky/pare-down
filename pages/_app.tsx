cf8ea82e40031d8ebb2105eb9d8d64946dd2558f
datguysheepy <36929645+datguysheepy@users.noreply.github.com>
2021-02-24 00:54:23 +0100

    webpack error fix so the app can be build now

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,9 @@ import { SWRConfig } from 'swr';
 import axios from 'axios';
 
 import { withToastProvider } from '@/toast';
-import { AppHead, GlobalStyles } from '../components/';
 
 const App: FC<AppProps> = ({ Component, pageProps }: AppProps) => (
 	<>
-		<AppHead />
-		<GlobalStyles />
 		<SWRConfig
 			value={{
 				fetcher: (url) => axios.get(url).then((res) => res.data),
